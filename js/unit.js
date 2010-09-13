5859cb12aae1795632e0c712756b3ddec9fe6fc4
develx <develx@thor>
2010-09-13 17:38:23 +0200

    added move API for unit

--- js/unit.js
+++ js/unit.js
@@ -17,6 +17,25 @@ Unit = (function () {
         this.player = player;
         this.color = color === undefined ?
                 Player.getPlayer(player).color : color;
+
+        this.move = function (distance) {
+            this.lastX = this.x;
+            this.lastY = this.y;
+            if ((this.direction >= 1) && (this.direction <= 3)) {
+                this.x += distance;
+            }
+            if (this.direction >= 5) {
+                this.x -= distance;
+            }
+            if ((this.direction == 7) || (this.direction <= 1)) {
+                this.y -= distance;
+            }
+            if ((this.direction >= 3) && (this.direction <= 5)) {
+                this.y += distance;
+            }
+            this.action = 2;
+        };
+
         units.push(this);
     };
 
