83319d3a7e2840ac2c931f3d45b7cd85b808ad89
develx <develx@thor>
2010-11-13 22:12:05 +0100

    implementation of terrain layer renderer

---
+++ js/terrainlayer.js
@@ -0,0 +1,65 @@
+var TerrainLayer;
+
+TerrainLayer = function () {
+    var buffer, canvas, canvasWidth, canvasHeight, canvasContext, map,
+            bufferWidth, bufferHeight;
+
+    this.setCanvas = function (newCanvas) {
+        canvas = newCanvas;
+        canvasWidth = canvas.width;
+        canvasHeight = canvas.height;
+        canvasContext = canvas.getContext('2d');
+    };
+
+    this.getBufferDimensions = function () {
+        return {
+            width: bufferWidth,
+            height: bufferHeight
+        };
+    };
+
+    this.setMap = function (newMap) {
+        if (!(newMap instanceof Array)) {
+            throw new TypeError('map has to be an array');
+        }
+        map = newMap;
+    };
+
+    this.display = function (x, y) {
+        canvasContext.putImageData(buffer.getImageData(x, y, canvasWidth,
+                canvasHeight), 0, 0);
+    };
+
+    this.init = function () {
+        var tileWidth, tileHeight, i, j, row, xOffset, context;
+        if (buffer) {
+            return;
+        }
+        if (!canvasContext) {
+            throw new Error("Missing renderer's target canvas");
+        }
+        if (!map) {
+            throw new Error('Cannot init terrain renderer without map data');
+        }
+        buffer = document.createElement('canvas');
+        tileWidth = TilesDefinition.getType(0).imageData.width;
+        tileHeight = TilesDefinition.getType(0).imageData.height;
+        context = buffer.getContext('2d');
+        tileHeight /= 2;
+        tileWidth -= 1;
+        tileHeight -= 1;
+        buffer.width = map[0].length * tileWidth + tileWidth / 2;
+        buffer.height = map.length * tileHeight + tileHeight;
+        for (i = map.length; i--;) {
+            row = map[i];
+            xOffset = (i % 2) * (tileWidth / 2);
+            for (j = row.length; j--;) {
+                context.drawImage(TilesDefinition.getType(row[j].type).
+                        imageData, xOffset + j * tileWidth, i * tileHeight);
+            }
+        }
+        bufferWidth = buffer.width;
+        bufferHeight = buffer.height;
+        buffer = context;
+    };
+};
