d3f147dc6f7135fb976b7e6119b5ec715531aec1
develx <develx@thor>
2011-02-12 19:40:45 +0100

    fixed units layer renderer

--- js/unitslayer.js
+++ js/unitslayer.js
@@ -4,9 +4,8 @@ require('settings', '../data/settings', '../data/unitsdefinition',
 
 UnitsLayer = function () {
     var canvas, canvasWidth, canvasHeight, canvasContext, tileWidth,
-            tileHeight, gridIndex, grid, addUnit, removeUnit, granularity,
-            canvasTileWidth, canvasTileHeight, canvasIndexWidth,
-            canvasIndexHeight, initConstants, displayGridCell;
+            tileHeight, grid, addUnit, removeUnit, gridWidth, gridHeight,
+            horizonzalTilesOnDisplay, verticalTilesOnDisplay;
 
     this.setCanvas = function (newCanvas) {
         canvas = newCanvas;
@@ -16,31 +15,31 @@ UnitsLayer = function () {
     };
 
     this.setMap = function (map) {
-        var i, j, row, cols, rows;
+        var i, j, row;
         if (!canvas) {
             throw new Error('The canvas has to be set before map');
         }
-        initConstants();
-        rows = Math.ceil(map.length / granularity);
-        cols = Math.ceil(map[0].length / granularity);
-        gridIndex = [];
-        for (i = rows; i--;) {
-            row = [];
-            for (j = cols; j--;) {
-                row.push(0);
-            }
-            gridIndex.push(row);
-        }
+        tileWidth = TilesDefinition.getType(0).imageData.width - 1;
+        tileHeight = TilesDefinition.getType(0).imageData.height / 2 - 1;
+        horizonzalTilesOnDisplay = Math.ceil(canvasWidth / tileWidth) + 2;
+        verticalTilesOnDisplay = Math.ceil(canvasHeight / tileHeight) + 2;
         grid = [];
-        rows = map.length;
-        cols = map[0].length;
-        for (i = rows; i--;) {
+        for (i = map.length; i--;) {
             row = [];
-            for (j = cols; j--;) {
+            for (j = map[0].length; j--;) {
                 row.push(false);
             }
             grid.push(row);
         }
+        gridWidth = grid[0].length;
+        gridHeight = grid.length;
+    };
+
+    this.getLayerDimensions = function () {
+        return {
+            width: gridWidth * tileWidth + tileWidth / 2,
+            height: gridHeight * tileHeight + tileHeight
+        };
     };
 
     this.onUnitChange = function (unit) {
@@ -57,77 +56,34 @@ UnitsLayer = function () {
     };
 
     this.display = function (x, y) {
-        var gridX, gridY, indexX, indexY, i, j;
+        var gridX, gridY, i, j, unit;
         canvasContext.globalCompositeOperation = 'destination-out';
         canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
         canvasContext.globalCompositeOperation = 'source-over';
-        gridX = Math.floor(x / tileWidth) - 1;
-        gridY = Math.floor(y / tileHeight) - 1;
-        gridX = Math.max(0, gridX);
-        gridY = Math.max(0, gridY);
-        indexX = Math.floor(gridX / granularity);
-        indexY = Math.floor(gridY / granularity);
-        for (i = canvasIndexHeight; i--;) {
-            for (j = canvasIndexWidth; j--;) {
-                if (gridIndex[j + indexY] &&
-                        gridIndex[j + indexY][i + indexX]) {
-                    displayGridCell(j + indexY, i + indexX, x, y);
-                }
-            }
-        }
-    };
-
-    displayGridCell = function (x, y, screenX, screenY) {
-        var i, j, type, unit;
-        x *= granularity;
-        y *= granularity;
-        for (i = granularity; i--;) {
-            for (j = granularity; j--;) {
-                if (grid[y + i][x + j]) {
-                    unit = grid[y + i][x + j];
-                    type = UnitsDefinition.getType(unit.type);
-                    screenX = (tileWidth / 2) * (unit.y % 2) + tileWidth *
-                            unit.x - screenX;
-                    screenY = (y + i) * unit.y - screenY;
-                    canvasContext.drawImage(type.imageData[unit.direction],
-                            screenX, screenY);
+        gridX = Math.max(0, Math.floor(x / tileWidth) - 1);
+        gridY = Math.max(0, Math.floor(y / tileHeight) - 1);
+        y -= 2;
+        for (i = verticalTilesOnDisplay; i--;) {
+            for (j = horizonzalTilesOnDisplay; j--;) {
+                if (grid[gridY + i] && (unit = grid[gridY + i][gridX + j])) {
+                    canvasContext.drawImage(UnitsDefinition.getType(unit.type).
+                            imageData[unit.direction],
+                            ((i + gridY) % 2) * tileWidth / 2 + (j + gridX) * tileWidth - x,
+                            (i + gridY) * tileHeight - y);
                 }
             }
         }
     };
 
     addUnit = function (unit) {
-        grid[unit.y][unit.y] = unit;
-        gridIndex[Math.ceil(unit.y / granularity)][Math.ceil(unit.x /
-                granularity)]++;
+        grid[unit.y][unit.x] = unit;
     };
 
     removeUnit = function (unit) {
-        var x, y;
         if (unit.action == 1) {
-            x = unit.x;
-            y = unit.y;
+            grid[unit.y][unit.x] = false;
         } else {
-            x = unit.lastX;
-            y = unit.lastY;
+            grid[unit.lastY][unit.lastX] = false;
         }
-        if (grid[y][x]) {
-            grid[y][x] = false;
-            gridIndex[Math.ceil(y / granularity)][Math.ceil(x /
-                    granularity)]--;
-        }
-    };
-
-    initConstants = function () {
-        tileWidth = TilesDefinition.getType(0).imageData.width;
-        tileHeight = TilesDefinition.getType(0).imageData.height;
-        tileHeight /= 2;
-        tileWidth -= 1;
-        tileHeight -= 1;
-        granularity = Settings.gridIndexGranularity;
-        canvasTileWidth = Math.ceil(canvasWidth / tileWidth) + 2;
-        canvasTileHeight = Math.ceil(canvasHeight / tileHeight) + 2;
-        canvasIndexWidth = Math.ceil(canvasTileWidth / granularity);
-        canvasIndexHeight = Math.ceil(canvasTileHeight / granularity);
     };
 };
