6a129e33a32d2a3758700589bc89105d7a105822
develx <develx@thor>
2011-02-16 18:22:04 +0100

    units layer renderer now uses a grid index to speed up rendering a bit

--- js/unitslayer.js
+++ js/unitslayer.js
@@ -5,7 +5,9 @@ require('settings', '../data/settings', '../data/unitsdefinition',
 UnitsLayer = function () {
     var canvas, canvasWidth, canvasHeight, canvasContext, tileWidth,
             tileHeight, grid, addUnit, removeUnit, gridWidth, gridHeight,
-            horizonzalTilesOnDisplay, verticalTilesOnDisplay;
+            horizonzalTilesOnDisplay, verticalTilesOnDisplay, index,
+            horizonzalIndexTilesOnDisplay, verticalIndexTilesOnDisplay,
+            indexGranularity, displayIndexCell;
 
     this.setCanvas = function (newCanvas) {
         canvas = newCanvas;
@@ -33,6 +35,19 @@ UnitsLayer = function () {
         }
         gridWidth = grid[0].length;
         gridHeight = grid.length;
+        index = [];
+        indexGranularity = Settings.gridIndexGranularity;
+        for (i = Math.ceil(gridHeight / indexGranularity); i--;) {
+            row = [];
+            for (j = Math.ceil(gridWidth) / indexGranularity; j--;) {
+                row.push(0);
+            }
+            index.push(row);
+        }
+        horizonzalIndexTilesOnDisplay =
+                Math.ceil(horizonzalTilesOnDisplay / indexGranularity);
+        verticalIndexTilesOnDisplay =
+                Math.ceil(verticalTilesOnDisplay / indexGranularity);
     };
 
     this.getLayerDimensions = function () {
@@ -56,24 +71,41 @@ UnitsLayer = function () {
     };
 
     this.display = function (x, y) {
-        var gridX, gridY, i, j, unit, currentY, currentX, offsetX;
+        var gridX, gridY, indexX, indexY, i, j;
         canvasContext.globalCompositeOperation = 'destination-out';
         canvasContext.fillRect(0, 0, canvasWidth, canvasHeight);
         canvasContext.globalCompositeOperation = 'source-over';
         gridX = Math.max(0, Math.floor(x / tileWidth) - 1);
         gridY = Math.max(0, Math.floor(y / tileHeight) - 1);
         y -= 2;
-        for (i = verticalTilesOnDisplay; i--;) {
+        indexX = Math.floor(gridX / indexGranularity);
+        indexY = Math.floor(gridY / indexGranularity);
+        for (i = verticalIndexTilesOnDisplay; i--;) {
+            if (index[indexY + i]) {
+                for (j = horizonzalIndexTilesOnDisplay; j--;) {
+                    if (index[indexY + i][indexX + j]) {
+                        displayIndexCell(indexX + j, indexY + i, x, y);
+                    }
+                }
+            }
+        }
+    };
+
+    displayIndexCell = function (x, y, screenX, screenY) {
+        var i, j, unit, gridX, gridY, currentX, currentY, offsetX;
+        gridX = x * indexGranularity;
+        gridY = y * indexGranularity;
+        for (i = indexGranularity; i--;) {
             currentY = gridY + i;
-            offsetX = ((currentY) % 2) * tileWidth / 2;
-            for (j = horizonzalTilesOnDisplay; j--;) {
+            offsetX = (currentY % 2) * tileWidth / 2;
+            for (j = indexGranularity; j--;) {
                 currentX = gridX + j;
                 if (grid[currentY] && (unit = grid[currentY][currentX])) {
                     canvasContext.drawImage(UnitsDefinition.getType(unit.type).
                             imageData[unit.direction],
-                            offsetX + currentX * tileWidth - x +
-                            unit.moveOffsetX,
-                            currentY * tileHeight - y + unit.moveOffsetY);
+                            offsetX + currentX * tileWidth - screenX +
+                            unit.moveOffsetX, currentY * tileHeight - screenY +
+                            unit.moveOffsetY);
                 }
             }
         }
@@ -81,13 +113,19 @@ UnitsLayer = function () {
 
     addUnit = function (unit) {
         grid[unit.y][unit.x] = unit;
+        index[Math.floor(unit.y / indexGranularity)][Math.floor(unit.x /
+                indexGranularity)]++;
     };
 
     removeUnit = function (unit) {
         if (unit.action == 1) {
             grid[unit.y][unit.x] = false;
+            index[Math.floor(unit.y / indexGranularity)][Math.floor(unit.x /
+                    indexGranularity)]--;
         } else {
             grid[unit.lastY][unit.lastX] = false;
+            index[Math.floor(unit.lastY / indexGranularity)
+                    ][Math.floor(unit.lastX / indexGranularity)]--;
         }
     };
 };
