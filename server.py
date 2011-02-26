ccec5cfd940e25c0e797bf8c3522bca5abc9fe95
DevelX <DevelX@PC2009>
2011-02-26 15:42:31 +0100

    running the server script will also open the web browser

--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 import BaseHTTPServer
 import CGIHTTPServer
+import webbrowser
 
 __author__="develx"
 __date__ ="$7.8.2010 18:26:00$"
@@ -10,6 +11,7 @@ def main():
     PORT = 8000
     Handler = CGIHTTPServer.CGIHTTPRequestHandler
     httpd = BaseHTTPServer.HTTPServer(("", PORT), Handler)
+    webbrowser.open("http://localhost:8000/");
     print "Running at port ", PORT
     print "Hit Ctrl+C to exit"
     httpd.serve_forever()
