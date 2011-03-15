e825705844388775e96e1034c49d530d0e12086b
develx <develx@thor>
2011-03-15 13:31:57 +0100

    The BuildingsLayer class documentation

--- js/buildingslayer.js
+++ js/buildingslayer.js
@@ -1,10 +1,19 @@
 var BuildingsLayer;
 require('../data/tilesdefinition', '../data/buildingsdefinition');
 
+/**
+ * Renderer of buildings in the UI.
+ */
 BuildingsLayer = function () {
     var buffer, canvas, canvasWidth, canvasHeight, canvasContext, bufferWidth,
             bufferHeight, tileWidth, tileHeight, rawBuffer;
 
+    /**
+     * Sets the canvas that will be used for rendering of the buildings layer.
+     *
+     * @param {HTMLCanvasElement} newCanvas The canvas that should be used for
+     *        rendering.
+     */
     this.setCanvas = function (newCanvas) {
         canvas = newCanvas;
         canvasWidth = canvas.width;
@@ -12,6 +21,12 @@ BuildingsLayer = function () {
         canvasContext = canvas.getContext('2d');
     };
 
+    /**
+     * Returns the dimensions of the inner buffer in pixels as a object.
+     *
+     * @return {Object} The object containing properties width and height
+     *         representing the dimensions of the inner buffer in pixels.
+     */
     this.getBufferDimensions = function () {
         return {
             width: bufferWidth,
@@ -19,6 +34,13 @@ BuildingsLayer = function () {
         };
     };
 
+    /**
+     * Sets the map that is to be rendered. The map itself is not rendered by
+     * this layer, it is used only to set up some properties of the renderer.
+     *
+     * @param {Array} map Raw map data (two-dimensional array of tiles
+     *        objects).
+     */
     this.setMap = function (map) {
         tileWidth = TilesDefinition.getType(0).imageData.width;
         tileHeight = TilesDefinition.getType(0).imageData.height;
@@ -34,6 +56,13 @@ BuildingsLayer = function () {
         rawBuffer = buffer.getImageData(0, 0, bufferWidth, bufferHeight);
     };
 
+    /**
+     * Handler of the event when new building is created and added to the map.
+     * This handler renders the building to the inner buffer so it can be
+     * displayed in the UI.
+     *
+     * @param {Building} building The building that has been added to the map.
+     */
     this.onBuildingAdded = function (building) {
         var type, x, y;
         type = BuildingsDefinition.getType(building.type);
@@ -44,6 +73,13 @@ BuildingsLayer = function () {
         rawBuffer = buffer.getImageData(0, 0, bufferWidth, bufferHeight);
     };
 
+    /**
+     * Handler of the event when a building is removed from the map (sold,
+     * destroyed, etc...). This handler removes the building from the inner
+     * buffer so that it will not be displayed in the UI anymore.
+     *
+     * @param {Building} building The building that has been removed.
+     */
     this.onBuildingRemoved = function (building) {
         var type, x, y;
         type = BuildingsDefinition.getType(building.type);
@@ -54,6 +90,13 @@ BuildingsLayer = function () {
         rawBuffer = buffer.getImageData(0, 0, bufferWidth, bufferHeight);
     };
 
+    /**
+     * Displays the relevant part of the inner buffer in the UI using the
+     * provided canvas element.
+     *
+     * @param {Number} x The X offset in pixels.
+     * @param {Number} y The Y offset in pixels.
+     */
     this.display = function (x, y) {
         canvasContext.putImageData(rawBuffer, -x, -y);
     };
