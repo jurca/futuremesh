db515ae658a3f4f945704e80ade64dbba869740a
develx <develx@thor>
2011-03-16 22:58:26 +0100

    mainview's class documentation

--- js/mainview.js
+++ js/mainview.js
@@ -2,9 +2,24 @@ var MainView;
 require('terrainlayer', 'buildingslayer', 'unitslayer', 'tile', 'building',
         'unit', 'map');
 
+/**
+ * Unifying class handling the rendering of terrain, buildings and units
+ * layers.
+ */
 MainView = function () {
     var terrainLayer, buildingsLayer, unitsLayer;
 
+    /**
+     * Sets rendering canvases for terrain layer, buildings layer and units
+     * layer. These canvases has to be distinct.
+     *
+     * @param {HTMLCanvasElement} terrainCanvas The canvas for the terrain
+     *        layer renderer.
+     * @param {HTMLCanvasElement} buildingsCanvas The canvas for the buildings
+     *        layer renderer.
+     * @param {HTMLCanvasElement} unitsCanvas The canvas for the units layer
+     *        renderer.
+     */
     this.setCanvases = function (terrainCanvas, buildingsCanvas, unitsCanvas) {
         terrainLayer = new TerrainLayer();
         terrainLayer.setCanvas(terrainCanvas);
@@ -14,6 +29,11 @@ MainView = function () {
         unitsLayer.setCanvas(unitsCanvas);
     };
 
+    /**
+     * Sets the map for the renderer layers.
+     *
+     * @param {Map} map The map instance or raw map data.
+     */
     this.setMap = function (map) {
         if (!terrainLayer) {
             throw new Error('Cannot set map before canvas');
@@ -27,12 +47,24 @@ MainView = function () {
         terrainLayer.init();
     };
 
+    /**
+     * Displays the view using all renderers.
+     *
+     * @param {Number} x The X-coordinate offset for renderers.
+     * @param {Number} y The Y-coordinate offset for renderers.
+     */
     this.display = function (x, y) {
         terrainLayer.display(x, y);
         buildingsLayer.display(x, y);
         unitsLayer.display(x, y);
     };
 
+    /**
+     * Event handler for changes about buildings - adding or removing of a
+     * building.
+     *
+     * @param {Building} building Building which status has changed.
+     */
     this.onBuildingChange = function (building) {
         if (building.type === false) {
             buildingsLayer.onBuildingRemoved(building);
@@ -41,10 +73,22 @@ MainView = function () {
         }
     };
 
+    /**
+     * Event handler for changes about units - adding, removing or unit
+     * movements.
+     *
+     * @param {Unit} unit The unit which status has changed.
+     */
     this.onUnitChange = function (unit) {
         unitsLayer.onUnitChange(unit);
     };
 
+    /**
+     * Returns dimensions of the layers' inner buffer in pixels.
+     *
+     * @return {Object} Object with properties width and height containing
+     *         dimensions of inner buffers in pixels.
+     */
     this.getLayersDimensions = function () {
         if (!terrainLayer) {
             throw new Error('Layers has not been initialized yet');
