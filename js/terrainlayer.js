1ad733f1b52146076aa3ee3547b2c7125de27cd0
DevelX <DevelX@PC2009>
2011-04-03 10:53:31 +0200

    The TerrainLayer class' documentation

--- js/terrainlayer.js
+++ js/terrainlayer.js
@@ -1,15 +1,35 @@
 var TerrainLayer;
 require('../data/tilesdefinition');
 
+/**
+ * Renderer of the terrain in the UI.
+ */
 TerrainLayer = function () {
     var buffer, canvas, canvasContext, map, bufferWidth, bufferHeight,
             rawBuffer;
 
+    /**
+     * Sets the canvas to be used to render the terrain in the UI.
+     *
+     * @param {HTMLCanvasElement} newCanvas The canvas to be used to render the
+     *        terrain in the UI.
+     */
     this.setCanvas = function (newCanvas) {
         canvas = newCanvas;
         canvasContext = canvas.getContext('2d');
     };
 
+    /**
+     * Returns a single object containing the information about inner buffer
+     * dimensions.
+     *
+     * @return {Object} The object containing the the information about inner
+     *         buffer dimensions. It has two properties:
+     *         <ul>
+     *             <li>width - the width of the buffer in pixels</li>
+     *             <li>height - the height of the buffer in pixels</li>
+     *         </ul>
+     */
     this.getBufferDimensions = function () {
         return {
             width: bufferWidth,
@@ -17,6 +37,12 @@ TerrainLayer = function () {
         };
     };
 
+    /**
+     * Sets the current map to be rendered.
+     *
+     * @param {Array} newMap The raw map content. Do not pass the Map class
+     *        instance, it won't work.
+     */
     this.setMap = function (newMap) {
         if (!(newMap instanceof Array)) {
             throw new TypeError('map has to be an array');
@@ -24,10 +50,25 @@ TerrainLayer = function () {
         map = newMap;
     };
 
+    /**
+     * Displays the terrain on the set canvas. Cannot be used before the init
+     * method.
+     *
+     * @param {Number} x The X offset of the terrain layer to be displayed.
+     * @param {Number} y The Y offset of the terrain layer to be displayed.
+     */
     this.display = function (x, y) {
         canvasContext.putImageData(rawBuffer, -x, -y);
     };
 
+    /**
+     * Initializes the terrain renderer. The methods setMap and setCanvas must
+     * be invoked before this method can be used, otherwise it will throw an
+     * Error.
+     * This methods creates it's own canvas, renders the map and then gets the
+     * raw content of the used canvas. This content then becomes the inner
+     * buffer that is used to display the terrain in the UI.
+     */
     this.init = function () {
         var tileWidth, tileHeight, i, j, row, xOffset, context;
         if (buffer) {
