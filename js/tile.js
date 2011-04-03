16e015ba2b2ee1aae6be9259f0c866dba5efe6d5
DevelX <DevelX@PC2009>
2011-04-03 11:01:46 +0200

    The Tile class' documentation

--- js/tile.js
+++ js/tile.js
@@ -1,11 +1,39 @@
 var Tile;
 require('../data/tilesdefinition');
 
+/**
+ * The Tile class representing a tile in the map. This class only holds details
+ * of the tile and image data used for rendering.
+ *
+ * @param {Number} type ID of the tile's type
+ */
 Tile = function (type) {
     var definition;
     definition = TilesDefinition.getType(type);
+    /**
+     * ID number of the tile's inner type.
+     *
+     * @type Number
+     */
     this.type = type;
+    /**
+     * Path to the image representing the tile in the game.
+     *
+     * @type String
+     */
     this.image = definition.image;
+    /**
+     * True if the tile is accessible by land units and buildings can be placed
+     * on it, otherwise false.
+     *
+     * @type Boolean
+     */
     this.accessible = definition.accessible;
+    /**
+     * The representation of the tile in the minimap specified as a
+     * CSS-compatible color definition.
+     *
+     * @type String
+     */
     this.minimap = definition.minimap;
 };
