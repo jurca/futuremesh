0808bb448e1480bdd4c18151c6a2c831f59732d4
develx <develx@thor>
2011-07-20 23:00:30 +0200

    implemented palettes of terrain tiles, buildings, units and sfx

--- mapeditor/mapeditor.js
+++ mapeditor/mapeditor.js
@@ -1,10 +1,11 @@
 require('imageloader', '../mapeditor/modal', '../mapeditor/progressbar',
-        '../mapeditor/mapeditorviewui', '../mapeditor/mapeditormainmenu');
+        '../mapeditor/mapeditorviewui', '../mapeditor/mapeditormainmenu',
+        '../mapeditor/mapeditorpallets');
 
 var MapEditor;
 
 MapEditor = function () {
-    var $, viewUI, defaultMapWidth, defaultMapHeight, map;
+    var $, viewUI, defaultMapWidth, defaultMapHeight, map, currentBrush;
     
     defaultMapWidth = 170;
     defaultMapHeight = 400;
@@ -18,6 +19,7 @@ MapEditor = function () {
     viewUI = new MapEditorViewUI();
     viewUI.setMap(map);
     new MapEditorMainMenu(this, defaultMapWidth, defaultMapHeight);
+    new MapEditorPallets(this);
 
     this.setMap = function (map) {
         viewUI.setMap(map);
@@ -26,21 +28,32 @@ MapEditor = function () {
     this.getMap = function () {
         return viewUI.getMap();
     };
+    
+    this.setCurrentBrush = function (brush) {
+        currentBrush = brush;
+    };
+    
+    this.getCurrentBrush = function () {
+        return currentBrush;
+    };
 };
 
 addEventListener('load', function () {
-    var modal, imageLoader, progressbar;
-    modal = new Modal('Loading images...');
-    progressbar = new Progressbar(0);
-    modal.appendChild(progressbar);
-    modal.center();
-    imageLoader = new ImageLoader();
-    imageLoader.addObserver(function (percent) {
-        progressbar.setValue(percent * 100);
-        if (percent == 1) {
-            new MapEditor();
-            modal.close();
-        }
-    });
-    imageLoader.load(BuildingsDefinition, UnitsDefinition, TilesDefinition);
+    setTimeout(function () {
+        var modal, imageLoader, progressbar;
+        modal = new Modal('Loading images...');
+        progressbar = new Progressbar(0);
+        modal.appendChild(progressbar);
+        modal.center();
+        imageLoader = new ImageLoader();
+        imageLoader.addObserver(function (percent) {
+            progressbar.setValue(percent * 100);
+            if (percent == 1) {
+                new MapEditor();
+                modal.close();
+            }
+        });
+        imageLoader.load(BuildingsDefinition, UnitsDefinition,
+            TilesDefinition);
+    }, 100);
 }, false);
