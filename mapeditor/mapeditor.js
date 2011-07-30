d982f0cfaae7af283ddb86fa9289f6ed66a87c99
DevelX <DevelX@PC2009>
2011-07-30 16:30:58 +0200

    map editor classes now use strict mode

--- mapeditor/mapeditor.js
+++ mapeditor/mapeditor.js
@@ -1,3 +1,4 @@
+"use strict";
 require('imageloader', '../mapeditor/modal', '../mapeditor/progressbar',
         '../mapeditor/mapeditorviewui', '../mapeditor/mapeditormainmenu',
         '../mapeditor/mapeditorpallets', '../mapeditor/mapeditormouse');
