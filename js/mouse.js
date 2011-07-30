640c459e2d705f3ceaff7de124f11a4ed287d11e
DevelX <DevelX@PC2009>
2011-07-30 16:16:57 +0200

    mouse utility that calculates over which map tile is located the mouse cursor

---
+++ js/mouse.js
@@ -0,0 +1,72 @@
+"use strict";
+var Mouse;
+
+Mouse = function () {
+    var x, y, mapX, mapY, canvasLeftOffset, canvasTopOffset, tileWidth,
+            tileHeight, tileWidthHalf, mapXOffset, mapYOffset;
+    
+    tileWidth = Math.floor(Settings.tileWidth);
+    tileHeight = Math.floor(Settings.tileHeight / 2);
+    tileWidthHalf = Math.floor(tileWidth / 2);
+    mapXOffset = 0;
+    mapYOffset = 0;
+    
+    this.getX = function () {
+        return x;
+    };
+    
+    this.getY = function () {
+        return y;
+    };
+    
+    this.getMapX = function () {
+        return mapX;
+    };
+    
+    this.getMapY = function () {
+        return mapY;
+    };
+    
+    this.setCanvasOffset = function (left, top) {
+        canvasLeftOffset = left;
+        canvasTopOffset = top;
+    };
+    
+    this.setMapOffset = function (x, y) {
+        mapXOffset = x;
+        mapYOffset = y;
+    };
+    
+    addEventListener('mousemove', function (e) {
+        var xShift, yShift;
+        x = e.pageX - canvasLeftOffset + mapXOffset;
+        y = e.pageY - canvasTopOffset + mapYOffset;
+        mapX = Math.floor(x / tileWidth);
+        mapY = Math.floor(y / tileHeight);
+        xShift = x - mapX * tileWidth;
+        yShift = y - mapY * tileHeight;
+        if (mapY % 2) {
+            if (xShift >= tileWidthHalf) {
+                xShift -= tileWidthHalf;
+                mapY -= (xShift + yShift < tileWidthHalf) ? 1 : 0;
+            } else {
+                if (tileWidthHalf - xShift + yShift < tileWidthHalf) {
+                    mapY--;
+                } else {
+                    mapX--;
+                }
+            }
+        } else {
+            if (xShift >= tileWidthHalf) {
+                xShift -= tileWidthHalf;
+                mapY -= (tileWidthHalf - xShift + yShift < tileWidthHalf) ?
+                        1 : 0;
+            } else {
+                if (xShift + yShift < tileWidthHalf) {
+                    mapX--;
+                    mapY--;
+                }
+            }
+        }
+    }, false);
+};
