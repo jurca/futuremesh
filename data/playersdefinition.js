bad8f7d24c5beb2a4434c9350a8f691c83367ce5
DevelX <DevelX@PC2009>
2011-07-31 16:27:55 +0200

    simple PlayersDefinition class defining some generic properties, the Player class now requires it, it's constructor uses new parameter type, parameters color and race are now optional, added static method createGenericPlayers that will create instances from the definitions (accessible through the getPlayer method)

---
+++ data/playersdefinition.js
@@ -0,0 +1,59 @@
+var PlayersDefinition;
+
+PlayersDefinition = (function () {
+    var types;
+    
+    types = [
+        {
+            type: 0,
+            color: '#ff3300',
+            race: 0
+        },
+        {
+            type: 1,
+            color: '#2de000',
+            race: 1
+        },
+        {
+            type: 2,
+            color: '#3300ff',
+            race: 2
+        },
+        {
+            type: 3,
+            color: '#b300ff',
+            race: 0
+        },
+        {
+            type: 4,
+            color: '#ffff00',
+            race: 1
+        },
+        {
+            type: 5,
+            color: '#00fff7',
+            race: 2
+        },
+        {
+            type: 6,
+            color: '#cfcfcf',
+            race: 0
+        },
+        {
+            type: 7,
+            color: '#8c6c00',
+            race: 1
+        },
+        {
+            type: 8,
+            color: '#000000',
+            race: 2
+        }
+    ];
+    
+    return {
+        getType: function (type) {
+            return types[type];
+        }
+    };
+}());
