2272e839fe11b6851f23b3f2775ca19db5b07b06
develx <develx@thor>
2011-09-18 10:38:54 +0200

    color of the SFX 3D light beams and buildings overlay is now configurable

--- data/settings.js
+++ data/settings.js
@@ -3,5 +3,7 @@ Settings.load({
     heightScale: 0.82,
     gridIndexGranularity: 5,
     sfx3DLightFactor: 1.5,
+    sfx3DLightColor: '#ffffff',
+    sfxBuildLayerColor: '#ff0000',
     mouseWheelSpeed: 0.4
 });
