e6a75d15f51b57b56131dd2c6456164f0fe2ff9a
develx <develx@thor>
2011-09-18 20:10:17 +0200

    early alpha version of map generator

---
+++ mapgenerator/forms.js
@@ -0,0 +1,42 @@
+"use strict";
+var Forms;
+
+Forms = function () {
+    var forms, initForm, i;
+    
+    this.getFormData = function (form) {
+        var inputs, data, i;
+        data = {};
+        inputs = form.getElementsByTagName('input');
+        for (i = inputs.length; i--;) {
+            data[inputs[i].name] = inputs[i].type == 'checkbox' ?
+                    inputs[i].checked : parseInt(inputs[i].value, 10);
+        }
+        inputs = form.getElementsByTagName('select');
+        for (i = inputs.length; i--;) {
+            data[inputs[i].name] = parseInt(inputs[i].value, 10);
+        }
+        return data;
+    };
+    
+    initForm = function (form) {
+        var inputs, i;
+        inputs = form.getElementsByTagName('input');
+        for (i = inputs.length; i--;) {
+            if (inputs[i].type == 'range') {
+                inputs[i].addEventListener('change', function () {
+                    var span;
+                    span = this.parentNode.getElementsByTagName('span')[0];
+                    span.innerHTML = this.value;
+                }, false);
+                inputs[i].parentNode.getElementsByTagName('span')[0].
+                        innerHTML = inputs[i].value;
+            }
+        }
+    };
+    
+    forms = document.getElementsByTagName('form');
+    for (i = forms.length; i--;) {
+        initForm(forms[i]);
+    }
+};
