6102af43378e66057bdbfce31eb230e2c6968379
develx <develx@thor>
2013-01-13 11:31:43 +0100

    added some royalty-free testing music for testing the game music daemon

--- data/settings.js
+++ data/settings.js
@@ -10,5 +10,41 @@ Settings.load({
     mouseWheelSpeed: 0.4,
     loadingMusic: ['data/music/loading.mp3', 'data/music/loading.ogg'],
     loadingMusicVolume: 0.4,
-    loadingMusicLength: 15.6745
+    loadingMusicLength: 15.6745,
+    gameMusic: {
+        playlists: {
+            0: [
+                {
+                    src: [ 'data/music/gameplay/babylon.ogg' ],
+                    duration: 251.4
+                },
+                {
+                    src: [ 'data/music/gameplay/winter-reflections.ogg' ],
+                    duration: 140.8
+                }
+            ],
+            0.5 : [
+                {
+                    src: [ 'data/music/gameplay/techno-dog.ogg' ],
+                    duration: 189.7
+                },
+                {
+                    src: [ 'data/music/gameplay/dance-zone.ogg' ],
+                    duration: 82.2
+                }
+            ],
+            1: [
+                {
+                    src: [ 'data/music/gameplay/fast-track.ogg' ],
+                    duration: 30.8
+                },
+                {
+                    src: [ 'data/music/gameplay/long-time-coming.ogg' ],
+                    duration: 274
+                }
+            ]
+        },
+        fadeDuration: 3500,
+        volume: 0.7
+    }
 });
