103bc1c540733fe55524cbb697d4255bc57bce97
develx <develx@thor>
2013-01-27 21:36:22 +0100

    added BuildingsConstructionUIPlugin for creating and managing building construction plugins. alpha version

---
+++ engine/gameplay-plugins/event-driven-plugins/buildingsconstructionuiplugin.js
@@ -0,0 +1,144 @@
+"use strict";
+var BuildingsConstructionUIPlugin;
+
+/**
+ * The BuildingsConstructionUIPlugin creates and handles UI buttons for
+ * starting construction of buildings and initiating their placement on the map
+ * once the construction process is finished.
+ */
+BuildingsConstructionUIPlugin = function () {
+    var currentPlayerRace, template, buttonsContainer, createButton,
+            createButtons, scaleButtonIcon, instance, playerId;
+    
+    instance = this;
+    
+    /**
+     * Scales the building construction button's icon image to fit the button
+     * while preserving the image's aspect ratio.
+     * 
+     * @param {Object} building Definition of the building for which
+     *        the button is to be created.
+     * @param {HTMLImageElement} img Image icon of the button.
+     */
+    scaleButtonIcon = function (building, img) {
+        var horizontalRatio, verticalRatio, shrinkRatio;
+        if ((building.imageWidth <= template.width) ||
+                (building.imageHeigth <= template.height)) {
+            img.style.width = building.imageWidth + 'px';
+            img.style.height = building.imageHeigth + 'px';
+        } else {
+            // preserve the image's aspect ratio
+            horizontalRatio = building.imageWidth / template.width;
+            verticalRatio = building.imageHeigth / template.height;
+            shrinkRatio = Math.max(horizontalRatio, verticalRatio);
+            img.style.width = building.imageWidth / shrinkRatio + 'px';
+            img.style.height = building.imageHeigth / shrinkRatio + 'px';
+        }
+    };
+    
+    /**
+     * Creates a building construction button and adds it to the UI.
+     * 
+     * @param {Object} building Definition of the building for which
+     *        the button is to be created.
+     */
+    createButton = function (building) {
+        var node, img, progressInfo;
+        node = document.createElement(template.tag);
+        node.className = template.className;
+        node.innerHTML = template.innerHTML;
+        
+        img = node.getElementsByTagName('img')[0];
+        img.src = building.imageData.src;
+        img.alt = building.name;
+        
+        progressInfo = node.getElementsByTagName('div')[0];
+        node.getElementsByTagName('div')[2].innerHTML =
+                building.name;
+        
+        // we have information about image's dimensions, so we can scale it
+        if (building.imageWidth && building.imageHeigth) {
+            scaleButtonIcon(building, img);
+        }
+        
+        node.addEventListener('click', function () {
+            progressInfo.innerHTML = '0 %';
+            instance.sendEvent('enqueueBuildingConstruction', {
+                player: playerId,
+                building: building.type
+            });
+        }, false);
+        
+        buttonsContainer.appendChild(node);
+    };
+    
+    /**
+     * Creates buttons for constructing new buildings in the UI.
+     */
+    createButtons = function () {
+        var building, type;
+        for (
+            type = 0;
+            building = BuildingsDefinition.getType(type);
+            type++
+        ) {
+            if ((building.race !== null) &&
+                    (building.race == currentPlayerRace)) {
+                createButton(building);
+            }
+        }
+    };
+    
+    /**
+     * Event handler for the <code>playerInitialization</code>. The event is
+     * sent by the GameLoader utility.
+     * 
+     * @param {Player} player The current human player controlling the UI
+     *        repesented as a Player instance.
+     */
+    this.onPlayerInitialization = function (player) {
+        playerId = player.id;
+        currentPlayerRace = player.race;
+        if (template) {
+            createButtons();
+        }
+    };
+    
+    /**
+     * Event handler for the <code>start</code> event. The event is sent by the
+     * GamePlay daemon when it is started and carries a <code>null</code> as
+     * its data.
+     * 
+     * @param {Object} tmp A <code>null</code> representing the event's data.
+     */
+    this.onStart = function (tmp) {
+        var templateNode;
+        templateNode = document.querySelector(
+                '#buildings-buttons .construction-buttons-content *')
+        buttonsContainer = templateNode.parentNode;
+        
+        template = {
+            tag: templateNode.nodeName,
+            className: templateNode.className,
+            innerHTML: templateNode.innerHTML,
+            width: 70,
+            height: 38
+        };
+        buttonsContainer.innerHTML = '';
+        if (currentPlayerRace) {
+            createButtons();
+        }
+    };
+    
+    /**
+     * Event handler for the <code>stop</code> event. The event is sent by the
+     * GamePlay daemon when it is stopped and carries a <code>null</code> as
+     * its data.
+     * 
+     * @param {Object} tmp A <code>null</code> representing the event's data.
+     */
+    this.onStop = function (tmp) {
+        buttonsContainer.innerHTML = '';
+    };
+};
+BuildingsConstructionUIPlugin.prototype = new AdvancedEventDrivenPlugin();
