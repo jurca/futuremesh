fa1f65b93641ad0d78380d91b02a48bc30cec6aa
develx <develx@thor>
2013-03-05 18:41:11 +0100

    sprite image loader update: added timestamp query parameter to image url. this should prevent browser cache issues

--- engine/images/spriteloader.js
+++ engine/images/spriteloader.js
@@ -4,7 +4,7 @@ var SpriteLoader;
 /**
  * The SpriteLoader is a highly-efficient alternative to the ImageLoader class.
  * The SpriteLoader loads all images using two HTTP requests:
- * 
+ *
  * <ol>
  *     <li>data/images.png - a sprite containing all unit, building and tile
  *         images.</li>
@@ -12,7 +12,7 @@ var SpriteLoader;
  *         extraction of the tile, building and unit images from the loaded
  *         sprite image.</li>
  * </ol>
- * 
+ *
  * The SpriteLoader then processes the loaded sprite, extract the images and
  * uses the Colorifier utility to create variants for each side.
  */
@@ -20,10 +20,10 @@ SpriteLoader = function () {
     var observers, notifyObservers, loadSprite, indexes, getBaseURL, canvas,
             context, baseUrl, extractTiles, tileWidth, tileHeight,
             extractImage, extractUnits, extractBuildings, colorifier;
-    
+
     observers = [];
     colorifier = new Colorifier();
-    
+
     /**
      * Add a new observer of the loading process. The loader executes all
      * registered observers every time the loading process progresses. An
@@ -31,7 +31,7 @@ SpriteLoader = function () {
      * to a floating-point number representing the current process, 0 being
      * the start, 1 meaning finished. The argument may be alternatively set to
      * -1 to indicate an error.
-     * 
+     *
      * @param {Function} observer Loading progress observer.
      */
     this.addObserver = function (observer) {
@@ -39,15 +39,15 @@ SpriteLoader = function () {
             observers.push(observer);
         }
     };
-    
+
     /**
      * Initializes the loading process. The SpriteLoader loads first the index
      * file, then loads the sprite, then extracts all images from the sprite
      * and uses the Colorifier to finish the process.
-     * 
+     *
      * <p>The loader executes the registered loading process observers each
      * time the process progresses.</p>
-     * 
+     *
      * @see #addObserver({Function} observer)
      */
     this.load = function () {
@@ -74,7 +74,7 @@ SpriteLoader = function () {
         xhr.open('GET', baseUrl + 'data/sprite.json', true);
         xhr.send(null);
     };
-    
+
     /**
      * Loads the sprite containing all images. The progress is set to 0.7 upon
      * completion and the loader continues then with extracting tile images.
@@ -103,9 +103,9 @@ SpriteLoader = function () {
         image.onsuspend = function () {
             notifyObservers(-1);
         }
-        image.src = baseUrl + 'data/images.png';
+        image.src = baseUrl + 'data/images.png?time=' + (new Date()).getTime();
     };
-    
+
     /**
      * Extracts tile images from the loaded sprite. The progress is set to 0.8
      * upon completion. The loader extracts unit images during the next step.
@@ -119,7 +119,7 @@ SpriteLoader = function () {
         notifyObservers(0.8);
         setTimeout(extractUnits, 0);
     };
-    
+
     /**
      * Extracts unit images from the loaded sprite and colorifies them. The
      * progress is set to 0.9 upon completion. The loader extracts building
@@ -142,7 +142,7 @@ SpriteLoader = function () {
         notifyObservers(0.9);
         setTimeout(extractBuildings, 0);
     };
-    
+
     /**
      * Extracts building images from the loaded sprite and colorifies them. The
      * progress is set to 1 upon completion (the loading process has finished).
@@ -162,10 +162,10 @@ SpriteLoader = function () {
         }
         notifyObservers(1);
     };
-    
+
     /**
      * Extracts sub-image from the loaded sprite image.
-     * 
+     *
      * @param {Number} x The X-coordinate of the left top corner of the
      *        extracted image within the sprite image.
      * @param {Number} y The Y-coordinate of the left top cornenr of the
@@ -185,13 +185,13 @@ SpriteLoader = function () {
         image.src = imageCanvas.toDataURL();
         return image;
     };
-    
+
     /**
      * Attempts to auto-detect the base URL of the whole application for
      * setting the correct URLs of the sprite and index file. The method
      * requires a script tag within the current document pointing to this file
      * (no compressed alternative).
-     * 
+     *
      * @return {String} Detected base URL or an empty string if the
      *         autodetection fails.
      */
@@ -208,10 +208,10 @@ SpriteLoader = function () {
         }
         return '';
     };
-    
+
     /**
      * Executes all progress observers and passes them the provided argument.
-     * 
+     *
      * @param {Number} progress Current loading progress. Should be a
      *        floating-point number within the range [0, 1], or -1.
      */
