c6a42ced8ee02e198e5ed655af412a72b3ef9d56
develx <develx@thor>
2013-11-17 13:15:19 +0100

    + the gameplay thread is now timed using the requestAnimationFrame + the scheduled plugin may handle sub-ticks now + the view renderer renders the UI even on sub-ticks to create a steady system load * re-built the futuremesh.js packed engine file

--- engine/gameplay-plugins/scheduledplugin.js
+++ engine/gameplay-plugins/scheduledplugin.js
@@ -5,7 +5,7 @@ var ScheduledPlugin;
  * A ScheduledPlugin is a GamePlay plugin that is executed in regular intervals
  * or "ticks" by GamePlay's background thread. All scheduled plugins extend
  * this class.
- * 
+ *
  * <p>A plugin extending this class must override (implement) the abstract
  * method <code>handleTick</code>.</p>
  */
@@ -17,43 +17,68 @@ ScheduledPlugin = function () {
      * @type GamePlay
      */
     var gamePlayInstance;
-    
+
     /**
      * This method is executed by the GamePlay's background thread on each
      * tick. The only exception to this rule is when the thread cannot execute
      * all plugins in time and starts to drop ticks.
-     * 
+     *
      * <p>The implementing plugin has to override this method.</p>
      */
     this.handleTick = function () {
         throw new Error('Not implemented yet.');
     };
-    
+
+    /**
+     * Handles a sub-tick occurrence. A sub-tick occurs if the
+     * <code>requestAnimationFrame</code> timer manages to execute the game
+     * logic thread in shorter intervals than is the current tick duration.
+     *
+     * @param {Number} tickFragment The current fragment of tick duration since
+     *        the last occurred tick. The fragment is always within the range
+     *        (0, 1) non-inclusive.
+     */
+    this.handleSubTick = function (tickFragment) {
+        // nothing to do by default
+    };
+
     /**
      * Returns <code>true</code> if this plugin should not be executed more
      * than once even if there are multiple ticks to handle. This is useful
      * for example for rendering plugins, because rendering is resource-costly
      * and re-rendering the screen multiple times would not have any added
      * value for the user.
-     * 
+     *
      * <p>Plugins that return <code>true</code> from this method have their
      * <code>handleTick()</code> method executed after all other plugins have
      * handled all ticks to execute.</p>
-     * 
+     *
      * <p>This method returns <code>false</code> by default and has to be
      * overriden to return anything else.</p>
-     * 
+     *
      * @return {Boolean} <code>true</code> if the plugin should not be executed
      * more than once even if there are multiple ticks to handle.
      */
     this.ignoresExtraTicks = function () {
         return false;
     };
-    
+
+    /**
+     * Returns <code>true</code> if this plugin should be executed event if the
+     * gameplay thread is executed in mid-tick. Such situation may occur if the
+     * <code>requestAnimationFrame</code> timer manages to execute the game
+     * logic thread in shorter intervals that is the current tick duration.
+     *
+     * @returns {Boolean} <code>true</code> if this plugin handles sub-ticks.
+     */
+    this.handlesSubTicks = function () {
+        return false;
+    };
+
     /**
      * Sends an event asynchronously. The event is delived in the GamePlay's
      * background thread.
-     * 
+     *
      * @param {String} eventName Name of the send event.
      * @param {Object} eventData Data submitted along with the event. This can
      *        be any data and it's up to the event-receiving plugins to handle
@@ -62,11 +87,11 @@ ScheduledPlugin = function () {
     this.sendEvent = function (eventName, eventData) {
         gamePlayInstance.sendEvent(eventName, eventData);
     };
-    
+
     /**
      * Sets the reference to the GamePlay instance. This method is executed by
      * the GamePlay instance.
-     * 
+     *
      * @param {GamePlay} gamePlay GamePlay instance.
      */
     this.setGamePlay = function (gamePlay) {
