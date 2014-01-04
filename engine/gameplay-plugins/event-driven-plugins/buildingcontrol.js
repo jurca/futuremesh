55a95791009006c867b5ab06338e57f177653866
develx <develx@thor>
2014-01-04 15:49:16 +0100

    the buildings representing resources are no longer selectable nor do they have their healthbars displayed on mouseover

--- engine/gameplay-plugins/event-driven-plugins/buildingcontrol.js
+++ engine/gameplay-plugins/event-driven-plugins/buildingcontrol.js
@@ -3,31 +3,31 @@ var BuildingControl;
 
 /**
  * The BuildingsControl handles responses user actions related to buildings.
- * 
+ *
  * @constructor
  */
 BuildingControl = function () {
     /**
      * SFX renderer.
-     * 
+     *
      * @type SFX
      */
     var sfx,
-       
+
     /**
      * The current map.
-     * 
+     *
      * @type Map
      */
     map,
-            
+
     /**
      * ID of the current human player.
-     * 
+     *
      * @type Number
      */
     playerId;
-    
+
     /**
      * Event handler for the <code>playerInitialization</code>. The event is
      * sent by the GameLoader utility.
@@ -38,37 +38,43 @@ BuildingControl = function () {
     this.onPlayerInitialization = function (player) {
         playerId = player.id;
     };
-    
+
     /**
      * Handler for the <code>mouseTileMove</code> event that occurrs when the
      * user moves the mouse cursor to another tile. The handlers notifies the
      * SFX renderer what (if any) building is located under the mouse cursor.
-     * 
+     *
      * @param {Object} data Event's details.
      */
     this.onMouseTileMove = function (data) {
-        var atTile;
+        var atTile, definition;
         atTile = map.getObjectAt(data.x, data.y);
         if (atTile instanceof Building) {
-            sfx.setMouseoverBuilding(atTile);
+            definition = BuildingsDefinition.getType(atTile.type);
+            if (definition.resource === null) {
+                sfx.setMouseoverBuilding(atTile);
+            }
         } else {
             sfx.setMouseoverBuilding(null);
         }
     };
-    
+
     /**
      * Handler for the <code>leftMouseButtonClick</code> event that occurrs
      * when the user clicks the left mouse button. The handler selects the
      * building located at the tile that was clicked.
-     * 
+     *
      * @param {Object} data Event's details.
      */
     this.onLeftMouseButtonClick = function (data) {
-        var atTile;
+        var atTile, definition;
         atTile = map.getObjectAt(data.x, data.y);
         if (atTile instanceof Building) {
             if (atTile.player === playerId) {
-                sfx.setSelectedBuilding(atTile);
+                definition = BuildingsDefinition.getType(atTile.type);
+                if (definition.resource === null) {
+                    sfx.setSelectedBuilding(atTile);
+                }
             }
         } else if (atTile instanceof Unit) {
             if (atTile.player === playerId) {
@@ -76,32 +82,32 @@ BuildingControl = function () {
             }
         }
     };
-    
+
     /**
      * Handler for the <code>rightMouseButtonClick</code> event occurring when
      * the user click the right mouse button. The handler deselects the
      * currently selected building.
-     * 
+     *
      * @param {Object} data Event's details.
      */
     this.onRightMouseButtonClick = function (data) {
         sfx.setSelectedBuilding(null);
     };
-    
+
     /**
      * Handler for the <code>gameMapInitialization</code> event. The handler
      * sets the map reference.
-     * 
+     *
      * @param {type} gameMap
      */
     this.onGameMapInitialization = function (gameMap) {
         map = gameMap;
     };
-    
+
     /**
      * Handler for the <code>viewReady</code> event. The handler sets the SFX
      * renderer reference.
-     * 
+     *
      * @param {View} newView The current view renderer.
      */
     this.onViewReady = function (newView) {
