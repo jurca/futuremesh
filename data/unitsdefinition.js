8808778db68b47dd5397fa0b065cf136e3a0c403
develx <develx@thor>
2014-01-19 15:42:25 +0100

    added heavy tank unit (fires two projectiles at once)

--- data/unitsdefinition.js
+++ data/unitsdefinition.js
@@ -135,6 +135,69 @@ UnitsDefinition = (function () {
                 stepProgress: 100, // 10 steps (1000 is complete construction)
                 stepDuration: 1   // +1 for resource request
             }
+        },
+        {
+            type: 2,
+            name: 'Heavy Tank',
+            image: 'data/images/units/heavy-tank/?.png',
+            speed: 500,
+            turnSpeed: 500,
+            hitpoints: 300, // how much damage can the unit take
+            colorify: '#007524',
+            colorifyDistance: 50,
+            resource: null,
+            harvestSpeed: 0, // hitpoints per tick
+            harvestEfficiency: 0, // gained resources per tick =
+                                  // harvestSpeed * harvestEfficiency
+            firingSpeed: 40, // rate of fire = 1000 / 50 * (1 / tick duration)
+            attackPower: 22, // hitpoints damage to the target per attack
+            attackRange: 8, // the range (in adjusted tile units) at which the
+                            // unit may attack its target.
+            projectileType: 1,
+            projectileDuration: 6, // how many ticks for the projectile to
+                                   // reach its target
+            visionRange: 12, // the distance at which the units is able to spot
+                             // other units or buildings
+            projectileOffsets: [ // start offsets for each of the directions
+                [ // north
+                    { x: 0.4, y: 0.3 },
+                    { x: 0.6, y: 0.3 }
+                ],
+                [ // north-east
+                    { x: 0.65, y: 0.35 },
+                    { x: 0.75, y: 0.45 }
+                ],
+                [ // east
+                    { x: 0.8, y: 0.5 },
+                    { x: 0.8, y: 0.7 }
+                ],
+                [ // south-east
+                    { x: 0.75, y: 0.75 },
+                    { x: 0.65, y: 0.85 }
+                ],
+                [ // south
+                    { x: 0.4, y: 0.85 },
+                    { x: 0.6, y: 0.85 }
+                ],
+                [ // south-west
+                    { x: 0.25, y: 0.75 },
+                    { x: 0.35, y: 0.85 }
+                ],
+                [ // west
+                    { x: 0.2, y: 0.5 },
+                    { x: 0.2, y: 0.7 }
+                ],
+                [ // north-west
+                    { x: 0.25, y: 0.45 },
+                    { x: 0.35, y: 0.35 }
+                ]
+            ],
+            race: 0,
+            construction: { // total cost: 150, duration: 30 ticks
+                step: [15],
+                stepProgress: 100, // 10 steps (1000 is complete construction)
+                stepDuration: 2   // +1 for resource request
+            }
         }
     ];
 
