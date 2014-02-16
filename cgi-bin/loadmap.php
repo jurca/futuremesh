ca3acae5b16bb148c59d07e8d83b3837212ee98a
develx <develx@thor>
2014-02-16 13:38:38 +0100

    added server-side level-3 compression that is automatically applied to level-2 compressed maps. the compression only converts the JSON array to binary data and vice-versa, thus saving a few bytes

--- cgi-bin/loadmap.php
+++ cgi-bin/loadmap.php
@@ -9,4 +9,10 @@ if (!isset($_GET['name'])) {
 $path = dirname(__DIR__) . DIRECTORY_SEPARATOR . "data" . DIRECTORY_SEPARATOR;
 $file = $path . "maps" . DIRECTORY_SEPARATOR . $_GET['name'];
 
-echo file_get_contents($file);
+$data = file_get_contents($file);
+if (substr($data, 0, 1) === '3') {
+    $packFormat = 'n*'; // unsigned 16-bit integer, big endian
+    $data = array_values(unpack($packFormat, substr($data, 1)));
+    $data = '2' . json_encode($data);
+}
+echo $data;
